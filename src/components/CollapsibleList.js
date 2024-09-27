11e652eb210d4ba9faca80f0fd0ab343a96e20a6
NaraeH <naraehyeon@gmail.com>
2024-09-27 16:58:28 +1000

    modified CollapsibleList component

--- src/components/CollapsibleList.js
+++ src/components/CollapsibleList.js
@@ -1,132 +1,48 @@
+import React, { createContext, useContext, useState } from 'react';
 import './../styles/CollapsibleList.css';
+import { VscChevronDown, VscChevronRight } from 'react-icons/vsc';
 
-// 현재코드: https://codepen.io/ryomario/pen/XWwRrej
-// 참고1(부트스트랩X): https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_treeview
-// 참고2(부트스트랩O): https://codepen.io/grzegorz-rogala/pen/YzjgYRa
+const CollapsibleListContext = createContext();
 
-// rsuite: https://codesandbox.io/p/sandbox/affectionate-swanson-67vm2h?file=%2Findex.js%3A15%2C21
-export function CollapsibleList(props) {
-  console.log("<ul>");
-  console.log(props);
-  console.log("<ul>");
+export const CollapsibleList = ({ children, defaultOpenLevels = 1 }) => {
+  return (
+    <CollapsibleListContext.Provider value={{ defaultOpenLevels }}>
+      <div className="collapsible-list">
+        {children}
+      </div>
+    </CollapsibleListContext.Provider>
+  );
+};
+
+const ListItem = ({ children, title, level = 1 }) => {
+  const { defaultOpenLevels } = useContext(CollapsibleListContext);
+  const [isOpen, setIsOpen] = useState(level <= defaultOpenLevels);
+  const hasChildren = React.Children.toArray(children).some(child => child.type === ListItem);
+
+  const toggleOpen = () => setIsOpen(!isOpen);
+
   return (
     <>
-      <ul className='na-collapsible-list'>
-        { props.children }
-      </ul>
+      <div className={`list-item ${isOpen ? 'open' : ''}`}>
+        <div className="list-item-header" onClick={toggleOpen} style={{ '--depth': level - 1 }}>
+          {hasChildren && (
+            <span className="toggle-icon">
+              {isOpen ? <VscChevronDown /> : <VscChevronRight />}
+            </span>
+          )}
+          <span className="list-item-content">{title || children}</span>
+        </div>
+        {hasChildren && isOpen && (
+        <div className="list-item-children">
+          {React.Children.map(children, child => 
+            child.type === ListItem ? React.cloneElement(child, { level: level + 1 }) : null
+          )}
+        </div>
+        )}
+      </div>    
     </>
   )
-}
+};
 
+CollapsibleList.ListItem = ListItem;
 
-function Ul({ children, id, className, ...props }) {
-  console.log("<li>");
-  console.log(props);
-  console.log("<li>");
-
-  return (
-    <li className='na-collapsible-list-item'>
-      {/* <a href='#' class='na-collapsible-list-link'></a> */}
-      {children}
-    </li>
-  )
-}
-
-function Li({ children, id, className, ...props }) {
-  console.log("<li>");
-  console.log(props);
-  console.log("<li>");
-
-  return (
-    <li className='na-collapsible-list-item'>
-      {/* <a href='#' class='na-collapsible-list-link'></a> */}
-      {children}
-    </li>
-  )
-}
-
-function Item({ children, id, className, ...props }) {
-  console.log("<li>");
-  console.log(props);
-  console.log("<li>");
-
-  return (
-    <li className='na-collapsible-list-item'>
-      {/* <a href='#' class='na-collapsible-list-link'></a> */}
-      {children}
-    </li>
-  )
-}
-
-
-// function collapse(willCollapse = true, callback = (list) => {}) {
-//   this.collapsing = true;
-//   const height = this.scrollHeight;
-//   const stateFrom = {height: height + 'px'};
-//   const stateTo = {height: '0px'};
-  
-//   let animateState = [stateFrom, stateTo];
-//   if(!willCollapse) animateState = [stateTo, stateFrom];
-  
-//   this.animate(animateState,{
-//     duration:500,
-//     iterations:1,
-//     easing:'ease-in-out'
-//   }).finished.then(val => {
-//     this.collapsing = false;
-//     if(callback && typeof(callback) == 'function'){
-//       callback(this);
-//     }
-//   });
-// }
-// const showActiveTab = (tab) => {
-//   if(tab){
-//     document.querySelector('#active-tab').textContent = tab.textContent;
-//   }
-// }
-// showActiveTab(document.querySelector('.list-item.active'));
-
-// document.querySelectorAll('.list li').forEach(li => {
-//     const list = li.querySelector('.list');
-//     if(list)list.collapse = collapse.bind(list);
-//     li.querySelector('.list-item').addEventListener('click', (event) => {
-//         // if not a list (just list-item)
-//         if(!list){
-//           document.querySelectorAll('.list-item').forEach(listItem => {
-//             listItem.classList.remove('active');
-//           });
-//           event.target.classList.add('active');
-//           showActiveTab(event.target);
-//           event.preventDefault();
-//           return;
-//         }
-//         // if list still collapsing
-//         if(list.collapsing){
-//             event.preventDefault();
-//             return;
-//         }
-//         // set list collapse or open
-//         if(!list.classList.contains('open')) {
-//             list.classList.add('open')
-//             list.querySelectorAll('.list').forEach(oList => oList.classList.remove('open'));
-//             list.parentElement.parentElement.querySelectorAll('& > li > .list.open').forEach(oList => {
-//               if(oList !== list){
-//                 oList.collapse(true, ls => {
-//                     ls.querySelectorAll('.list').forEach(oList => oList.classList.remove('open'));
-//                     ls.classList.remove('open');
-//                 });
-//               }
-//             });
-//             // open
-//             list.collapse(false);
-//         } else {
-//             // collapse
-//             list.collapse(true, ls => {
-//                 ls.querySelectorAll('.list').forEach(oList => oList.classList.remove('open'));
-//                 ls.classList.remove('open');
-//             });
-//         }
-//     })
-// })
-
-CollapsibleList.Item = Item;
